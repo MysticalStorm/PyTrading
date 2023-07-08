5dc88822ea413bf5c5e0fa87bb69ec47b06e084e
DeadLine <mysticalstorming@gmail.com>
2023-07-09 01:47:30 +0300

    Add hash to css and js

--- web/core.py
+++ web/core.py
@@ -9,9 +9,18 @@ from trading.binance.core import Binance
 from trading.trading_manager import TradingManager
 from trading.models.kline import KLine
 from typing import Optional
+import hashlib
+import os
 
 
 # https://pgjones.gitlab.io/quart/how_to_guides/sync_code.html
+
+def calculate_file_hash(file_path):
+    with open(file_path, 'rb') as file:
+        content = file.read()
+        hash_object = hashlib.md5(content)
+        return hash_object.hexdigest()
+
 
 class WebCore:
     manager: TradingManager = None
@@ -57,8 +66,18 @@ class WebCore:
             if message:
                 message = message[0].content
             tickers = await self.manager.tickers()
-            currencies = [(ticker.name, 1) for ticker in tickers]
-            return await render_template('home.html', message=message, currencies=currencies)
+            currencies = [(ticker.name, "") for ticker in tickers]
+
+            # Calculate the hash of the CSS file
+            css_path = os.path.join(app.static_folder, 'css', 'main.css')
+            css_hash = calculate_file_hash(css_path)
+
+            # Calculate the hash of the JS file
+            js_path = os.path.join(app.static_folder, 'js', 'main.js')
+            js_hash = calculate_file_hash(js_path)
+
+            return await render_template('home.html', message=message, currencies=currencies, css_hash=css_hash,
+                                         js_hash=js_hash)
 
         @app.route('/stream')
         async def stream():
